a4ded6861c9a9b837373093bc0d9dba2b9feadb7
ashaqsolihjou-byte <ashaqsolihjou@gmail.com>
2025-12-16 13:32:37 +0330

    add service worker (sw.js) for offline support

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'euro-tactic-2d-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/game.js',
+  // You can add 'icon-192.png' and 'icon-512.png' here once they are added
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
